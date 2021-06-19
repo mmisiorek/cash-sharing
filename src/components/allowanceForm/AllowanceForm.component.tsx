b7b485187223c577f28e39a54dba5497234e14c0
Rskrzypczyk <rskrzypczyk@contractor.worldremit.com>
2021-06-19 17:02:38 +0200

    change tabs logic

--- src/components/allowanceForm/AllowanceForm.component.tsx
+++ src/components/allowanceForm/AllowanceForm.component.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 
 import { Box } from "@material-ui/core";
-import ShareForm from "../shareForm/ShareForm.component";
 
 const AllowanceForm: React.FC<any> = () => {
-  return <ShareForm />;
+  return <Box>TODO</Box>;
 };
 
 export default AllowanceForm;
--- src/components/transferForm/TransferForm.component.tsx
+++ src/components/transferForm/TransferForm.component.tsx
@@ -1,9 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { Box } from "@material-ui/core";
+import { Box, Button, TextField, Typography } from "@material-ui/core";
+import { getInputValue } from "./TransferForm.utils";
+import { Users } from "../Users";
+
+import Toggle from "../toggle/toggle.component";
+
+import { useTheme } from "@material-ui/core/styles";
 
 const TransferForm: React.FC<any> = () => {
-  return <Box>TODO transfer form</Box>;
+  const [value, setValue] = useState("");
+  const { palette } = useTheme();
+
+  const items = [
+    {
+      id: "1",
+      name: "Wprowadź wartości ręcznie",
+    },
+    {
+      id: "2",
+      name: "Wylicz wartości automatycznie",
+    },
+  ];
+  const [selectToggle, setSelectedToggle] = useState(items[0].id);
+
+  const valueChangeHandler = (
+    event: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+  ): void => {
+    const value = getInputValue(event?.currentTarget?.value, 2);
+
+    if (value === "" || value) {
+      setValue(value);
+    }
+  };
+
+  const onToggleChange = (value: string) => {
+    setSelectedToggle(value);
+  };
+
+  return (
+    <Box display="flex" justifyContent="center" pt={2}>
+      <Box p={2} bgcolor={palette.secondary.main} width="100%">
+        <Toggle toggles={items} onToggleChange={onToggleChange} />
+
+        {selectToggle === items[0].id && (
+          <>
+            <Box width="100%" pt={2} pb={2}>
+              <Users />
+            </Box>
+
+            <Box pt={2} display="flex" width="100%" justifyContent="center">
+              <Box>
+                <Typography variant="h4">Całkowita kwota</Typography>
+                <Typography color="textSecondary">Podaj kwotę</Typography>
+                <TextField
+                  value={value}
+                  onChange={valueChangeHandler}
+                  variant="outlined"
+                />
+              </Box>
+            </Box>
+
+            <Box pt={2} width="100%" display="flex" justifyContent="center">
+              <Button color="primary" variant="contained">
+                Dodaj odbiorce
+              </Button>
+            </Box>
+          </>
+        )}
+      </Box>
+    </Box>
+  );
 };
 
 export default TransferForm;
