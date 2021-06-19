71ef2d38faaf4564e6a1487e491d7554b4ab4433
Marcin Misiorek <marcin@misiorek.net>
2021-06-19 13:02:24 +0200

    Remvoe message reducer

--- packages/web/src/store/root.ts
+++ packages/web/src/store/root.ts
@@ -1,9 +1,7 @@
 import { combineReducers } from "@reduxjs/toolkit";
 
-import { messagesReducer } from "./messages";
 import { usersReducer } from "./users";
 
 export const rootReducer = combineReducers({
-  messages: messagesReducer,
   users: usersReducer,
 });
